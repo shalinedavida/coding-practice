55ceee19bb29f2c5626d5991adf6adbd60fa3eb0
Shaline Davida <shalinedavida@gmail.com>
2025-06-05 16:09:40 +0300

    added a new code to the file

--- instance.js
+++ instance.js
@@ -2,47 +2,45 @@
 // function newFunction(){
  
 // }
-// const checkInstance =new newFunction();
-// console.log(checkInstance instanceof newFunction);
-// console.log(checkInstance instanceof Object);
-// console.log(typeof newFunction ==="function");
+const checkInstance =new newFunction();
+console.log(checkInstance instanceof newFunction);
+console.log(checkInstance instanceof Object);
+console.log(typeof newFunction ==="function");
 
-// newFunction();
+newFunction();
 
 // Removing duplicates from a sorted array
-// function sortArray(){
-// //     const 
-// // }
 
-// function removeDuplicates(nums) {
-//   if (nums.length <= 1) return nums.length;
 
-//   let uniqueIndex = 0;
-//   for (let i = 1; i < nums.length; i++) {
-//     if (nums[i] !== nums[uniqueIndex]) {
-//       uniqueIndex++;
-//       nums[uniqueIndex] = nums[i];
-//     }
-//   }
-//   return uniqueIndex + 1;
-// }
+function removeDuplicates(nums) {
+  if (nums.length <= 1) return nums.length;
+
+  let uniqueIndex = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[uniqueIndex]) {
+      uniqueIndex++;
+      nums[uniqueIndex] = nums[i];
+    }
+  }
+  return uniqueIndex + 1;
+}
 
 // function removeDuplicates(3);
 
-// class Payment {
-//   constructor(payerAccount,payeeAccount,amount){
-//     this.payerAccount = payerAccount;
-//     this.payeeAccount = payeeAccount;
-//     this.amount = amount;
-//   }
-//   processPayment(){
-//     console.log(`Processing payment of ${this.amount} from ${this.payerAccount} , to ${this.payeeAccount} `);
+class Payment {
+  constructor(payerAccount,payeeAccount,amount){
+    this.payerAccount = payerAccount;
+    this.payeeAccount = payeeAccount;
+    this.amount = amount;
+  }
+  processPayment(){
+    console.log(`Processing payment of ${this.amount} from ${this.payerAccount} , to ${this.payeeAccount} `);
     
-//   }
-// };
+  }
+};
 
-// const payment = new Payment("ACC123","ACC456",1500);
-// payment.processPayment();
+const payment = new Payment("ACC123","ACC456",1500);
+payment.processPayment();
 
 class Product{
   constructor(name,quantity,amount){
